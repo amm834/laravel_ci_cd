ffaa4134f90e9035ca628f905741dba6e89090af
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2021-03-10 10:45:30 +0630

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-/*
+
 class ExampleTest extends TestCase
 {
     /**
@@ -12,11 +12,12 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
+/*
     public function testBasicTest()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
     }
-}
 */
+}
